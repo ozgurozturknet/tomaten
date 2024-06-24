b7fafb44fdb1b9cdf10cb33c351b6bda88d8d561
Ozgur Ozturk <ozgur@ozgurozturk.net>
2024-06-24 13:11:34 +0200

    skafold olarak web sitesi olusturuldu

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+#add a skafold of the main.py file  
+from flask import Flask
+
 print "hello world"
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
---
+++ test.py
@@ -0,0 +1,6 @@
+def test():
+    # Test cases go here
+    pass  # Remove this line and add your test cases
+
+# Call the test function
+test()
